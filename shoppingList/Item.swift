8bf4638932b370fce9c500f7954a1247b0197c33
Julio Rivera <jrivera0325@jh-mba158-16.local>
2019-02-12 11:27:16 -0600

    added Item class

---
+++ shoppingList/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  shoppingList
+//
+//  Created by Julio Rivera on 2/12/19.
+//  Copyright © 2019 John Hersey High School. All rights reserved.
+//
+
+import Foundation
+class Item {
+    var name : String
+    init(name: String) {
+        self.name = name
+    }
+    
+}
--- shoppingList/ViewController.swift
+++ shoppingList/ViewController.swift
@@ -8,13 +8,28 @@
 //Test
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var newItemTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self 
     }
 
-
+    @IBAction func addNewItemButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")!
+        cell.textLabel?.text = "Hello"
+        return cell
+    }
+    
 }
 
