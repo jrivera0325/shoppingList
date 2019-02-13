dc3f9523ba892266a00cae0bf4d554d1f64d7423
Julio Rivera <jrivera0325@jh-mba158-16.local>
2019-02-13 11:42:32 -0600

    added Quantity

--- shoppingList/Item.swift
+++ shoppingList/Item.swift
@@ -9,8 +9,10 @@
 import Foundation
 class Item {
     var name : String
-    init(name: String) {
+    var quantity: Int
+    init(name: String, quantity: Int) {
         self.name = name
+        self.quantity = quantity
     }
     
 }
--- shoppingList/ViewController.swift
+++ shoppingList/ViewController.swift
@@ -18,15 +18,20 @@ class ViewController: UIViewController, UITableViewDataSource {
         super.viewDidLoad()
         tableView.dataSource = self
         
-        let item1 = Item(name: "Milk")
-        let item2 = Item(name: "Eggs")
+        let item1 = Item(name: "Milk", quantity: 3)
+        let item2 = Item(name: "Eggs", quantity: 12)
         items = [item1, item2]
-        let item3 = Item(name: "Bread")
+        let item3 = Item(name: "Bread", quantity: 24)
         items.append(item3)
         
     }
-
+    
     @IBAction func addNewItemButtonPressed(_ sender: UIBarButtonItem) {
+        if let newItemName = newItemTextField.text, newItemName != "" {
+            let newItem = Item(name: newItemName, quantity: 1)
+            items.append(newItem)
+            tableView.reloadData()
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -34,11 +39,14 @@ class ViewController: UIViewController, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")!
-        let currentItemName = items[indexPath.row].name
-        cell.textLabel?.text = currentItemName
-        return cell
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "myCell") {
+            let currentItemName = items[indexPath.row].name
+            cell.textLabel?.text = currentItemName
+            return cell
+        } else {
+            return UITableViewCell()
+        }
+        
     }
     
 }
-
